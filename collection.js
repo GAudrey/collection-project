250795c0cfb978fba4a5cf8553143f9abbc24128
GAudrey <gilmantaudrey.ag@gmail.com>
2020-11-12 12:27:26 +0100

    some text corrected, add js collection

---
+++ collection.js
@@ -0,0 +1,113 @@
+const collection = [
+    {//Part Murdered: Soul Suspect
+        title: 'Murdered: Soul Suspect',
+        year: '2014',
+        category: ['Investigation', 'Stealth'],
+        developer: 'Airtight Games',
+        publisher: 'Square Enix',
+        description: 'Most people think that death is the end, but for Ronan O’Connor, a Salem police detective with a chequered past, it is just the beginning. When Ronan is caught in a violent burglary, his life is brought to an untimely and brutal end. Shocked to find himself in the afterlife, his only escape from the limbo world of ‘Dusk’ is to hunt down his killer using new-found supernatural abilities. Set in Salem Massachusetts, players have the freedom to explore the New England-style wooden houses, narrow streets, gothic churches and historical landmarks for clues. They will need to battle demonic spirits to preserve their souls whilst uncovering the shocking revelations about who is responsible for their death.',
+        website: 'https://square-enix-games.com/en_GB/games/murdered/',
+        trailer: 'https://www.youtube.com/watch?v=1xH4_YGEBPo',
+    },
+
+    {//Part The Talos Principle
+        title: 'The Talos Principle',
+        year: '2014',
+        category: ['Puzzle', 'Philosophy'],
+        developer: 'Croteam',
+        publisher: 'Devolver Digital',
+        description: 'The Talos Principle is a philosophical first-person puzzle game from Croteam, the creators of the legendary Serious Sam series, written by Tom Jubert (FTL, The Swapper) and Jonas Kyratzes (The Sea Will Claim Everything). As if awakening from a deep sleep, you find yourself in a strange, contradictory world of ancient ruins and advanced technology. Tasked by your creator with solving a series of increasingly complex puzzles, you must decide whether to have faith, or to ask the difficult questions: Who are you? What is your purpose And what are you going to do about it?',
+        website: 'http://www.croteam.com/talosprinciple/',
+        trailer: 'https://www.youtube.com/watch?v=Vu9QFBWb7WQ',
+    },
+
+    {//Part This War of Mine
+        title: 'This War of Mine',
+        year: '2014',
+        category: ['Management', 'Survival'],
+        developer: '11 bit studios',
+        publisher: '11 bit studios',
+        description: 'In This War of Mine for the first time you do not play as an elite soldier, but a group of civilians trying to survive in a besieged city; struggling with lack of necessities and constant danger.',
+        website: 'https://www.thiswarofmine.com/#description',
+        trailer: 'https://www.youtube.com/watch?v=BALBUyoTxQM',
+    },
+
+    {//Part Batman: Arkham Knight
+        title: 'Batman: Arkham Knight',
+        year: '2015',
+        category: ['Action', 'Adventure'],
+        developer: 'Rocksteady Studios',
+        publisher: 'Warner Bros. Interactive Entertainment',
+        description: 'Batman™: Arkham Knight brings the award-winning Arkham trilogy from Rocksteady Studios to its epic conclusion. Developed exclusively for New-Gen platforms, Batman: Arkham Knight introduces Rocksteady\'s uniquely designed version of the Batmobile. The highly anticipated addition of this legendary vehicle, combined with the acclaimed gameplay of the Arkham series, offers gamers the ultimate and complete Batman experience as they tear through the streets and soar across the skyline of the entirety of Gotham City. In this explosive finale, Batman faces the ultimate threat against the city that he is sworn to protect, as Scarecrow returns to unite the super criminals of Gotham and destroy the Batman forever.',
+        website: 'https://www.warnerbros.com/games-and-apps/batman-arkham-knight',
+        trailer: 'https://www.youtube.com/watch?v=bLv8xko2teg',
+    },
+
+    {//Part Plague Inc.: Evolved
+        title: 'Plague Inc.: Evolved',
+        year: '2016',
+        category: ['Simulation', 'Strategy'],
+        developer: 'Ndemic Creations',
+        publisher: 'Ndemic Creations',
+        description: 'Your pathogen has just infected \'Patient Zero\'. Now you must bring about the end of human history by evolving a deadly, global Plague whilst adapting against everything humanity can do to defend itself. Plague Inc: Evolved is a unique mix of high strategy and terrifyingly realistic simulation which is now available on PC, Mac and Linux via Steam. Over 130 million players have been infected by Plague Inc. already. Now, Plague Inc: Evolved combines the original critically acclaimed gameplay with significant, all-new features for PC, including multiplayer, user-generated content support, improved graphics and lots more. Get ready to take strategy gaming to the next level!',
+        website: 'https://www.ndemiccreations.com/en/25-plague-inc-evolved',
+        trailer: 'https://www.youtube.com/watch?v=pSat_gLDXPc',
+    },
+
+    {//Part Cat Quest
+        title: 'Cat Quest',
+        year: '2017',
+        category: ['Action', 'RPG'],
+        developer: 'The Gentlbros',
+        publisher: 'PQube Limited',
+        description: 'Cat Quest is a 2d Open World RPG by The Gentlebros. Set in the fantastic and wonderful world of cats, play as a catventurer as you explore a massive continent crafted in the unique style of tapestry! Relive the good old days of exploring an overworld map as you raid dungeons for epic loot, complete quests and meet the many furry denizens of this world! Also check out the sequel!',
+        website: 'https://www.thegentlebros.com/catquest/',
+        trailer: 'https://www.youtube.com/watch?v=asDUOd5IZH0',
+    },
+
+    {//Part The First Tree
+        title: 'The First Tree',
+        year: '2017',
+        category: ['Exploration', 'Storytelling'],
+        developer: 'David Wehle',
+        publisher: 'David Wehle',
+        description: 'A third-person exploration game centered around two parallel stories: a fox trying to find her missing family, and a young couple dealing with a tragedy in theirs. Players take control of the fox on a poignant and beautiful journey that crescendos at the source of life, and perhaps results in the understanding of death. Along the way, players can uncover artifacts and stories from the young couple’s life as they too become intertwined in the fox’s journey towards The First Tree.',
+        website: 'https://www.thefirsttree.com/',
+        trailer: 'https://www.youtube.com/watch?v=zXgnTKzVD4E',
+    },
+
+    {//Part Monster: Hunter World
+        title: 'Monster: Hunter World',
+        year: '2018',
+        category: ['Action', 'RPG'],
+        developer: 'Capcom',
+        publisher: 'Capcom',
+        description: 'As a hunter, you\'ll take on quests to hunt monsters in a variety of habitats. Take down these monsters and receive materials that you can use to create stronger weapons and armor in order to hunt even more dangerous monsters.',
+        website: 'http://www.monsterhunterworld.com/pc/fr/',
+        trailer: 'https://www.youtube.com/watch?v=Ro6r15wzp2o',
+    },
+
+    {//Part Spiritfarer
+        title: 'Spiritfarer',
+        year: '2020',
+        category: ['Management', 'Adventure'],
+        developer: 'Thunder Lotus Games',
+        publisher: 'Thunder Lotus Games',
+        description: 'Play as Stella, ferrymaster to the deceased, a Spiritfarer. Build a boat to explore the world, then befriend and care for spirits before finally releasing them into the afterlife.',
+        website: 'https://thunderlotusgames.com/spiritfarer/',
+        trailer: 'https://www.youtube.com/watch?v=6mfdampW-EQ',
+    },
+
+    {//Part Phasmophobia
+        title: 'SPhasmophobia',
+        year: '2020',
+        category: ['Horror', 'Investigation'],
+        developer: 'Kinetic Games',
+        publisher: 'Kinetic Games',
+        description: 'Phasmophobia is an online 4 player co-op horror with VR support. Paranormal activity is on the rise and it is up to your team to investigate and end the nightmare.',
+        website: 'https://kineticgames.co.uk/',
+        trailer: 'https://www.youtube.com/watch?v=sRa9oeo5KiY',
+    },
+]
+
+
