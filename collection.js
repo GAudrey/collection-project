57d161c723ec1af599e49b92193380520a55be7b
GAudrey <gilmantaudrey.ag@gmail.com>
2020-11-13 10:30:25 +0100

    try loop for 'tags'

--- collection.js
+++ collection.js
@@ -1,10 +1,83 @@
+/*tags array*/
+const category = [
+    {//Tag Investigation
+        name: 'Investigation',
+        style: 'has-text-weight-bold tag is-warning',
+    },
+
+    {//Tag Stealth
+        name: 'Stealth',
+        style: 'has-text-weight-bold tag is-dark',
+    },
+
+    {//Tag Puzzle
+        name: 'Puzzle',
+        style: 'has-text-weight-bold tag is-info',
+    },
+
+    {//Tag Philosophy
+        name: 'Philosophy',
+        style: 'has-text-weight-bold tag is-light',
+    },
+
+    {//Tag Management
+        name: 'Management',
+        style: 'has-text-weight-bold tag is-link',
+    },
+
+    {//Tag Survival
+        name: 'Survival',
+        style: 'has-text-weight-bold tag is-danger',
+    },
+
+    {//Tag Action
+        name: 'Action',
+        style: 'has-text-weight-bold tag is-danger is-light',
+    },
+
+    {//Tag Adventure
+        name: 'Adventure',
+        style: 'has-text-weight-bold tag is-success',
+    },
+
+    {//Tag Simulation
+        name: 'Simulation',
+        style: 'has-text-weight-bold tag is-info is-light',
+    },
+
+    {//Tag Strategy
+        name: 'Strategy',
+        style: 'has-text-weight-bold tag is-warning is-light',
+    },
+
+    {//Tag RPG
+        name: 'RPG',
+        style: 'has-text-weight-bold tag is-primary is-light',
+    },
+
+    {//Tag Exploration
+        name: 'Exploration',
+        style: 'has-text-weight-bold tag is-primary',
+    },
+
+    {//Tag Storytelling
+        name: 'Storytelling',
+        style: 'has-text-weight-bold tag is-white',
+    },
+
+    {//Tag Horror
+        name: 'Horror',
+        style: 'has-text-weight-bold tag is-black',
+    },
+];
+
 /* Collection array */
 const collection = [
     {//Part Murdered: Soul Suspect
         img: './imgs/illu-murdered-soul-suspect.jpg',
         title: 'Murdered: Soul Suspect',
         year: '2014',
-        category: ['Investigation', 'Stealth'],
+        category: category[0],
         developer: 'Airtight Games',
         publisher: 'Square Enix',
         description: 'Most people think that death is the end, but for Ronan O’Connor, a Salem police detective with a chequered past, it is just the beginning. When Ronan is caught in a violent burglary, his life is brought to an untimely and brutal end. Shocked to find himself in the afterlife, his only escape from the limbo world of ‘Dusk’ is to hunt down his killer using new-found supernatural abilities. Set in Salem Massachusetts, players have the freedom to explore the New England-style wooden houses, narrow streets, gothic churches and historical landmarks for clues. They will need to battle demonic spirits to preserve their souls whilst uncovering the shocking revelations about who is responsible for their death.',
@@ -17,7 +90,7 @@ const collection = [
         img: './imgs/illu-the-talos-principle.jpg',
         title: 'The Talos Principle',
         year: '2014',
-        category: ['Puzzle', 'Philosophy'],
+        category: category[0],
         developer: 'Croteam',
         publisher: 'Devolver Digital',
         description: 'The Talos Principle is a philosophical first-person puzzle game from Croteam, the creators of the legendary Serious Sam series, written by Tom Jubert (FTL, The Swapper) and Jonas Kyratzes (The Sea Will Claim Everything). As if awakening from a deep sleep, you find yourself in a strange, contradictory world of ancient ruins and advanced technology. Tasked by your creator with solving a series of increasingly complex puzzles, you must decide whether to have faith, or to ask the difficult questions: Who are you? What is your purpose And what are you going to do about it?',
@@ -30,7 +103,7 @@ const collection = [
         img: './imgs/illu-this-war-of-mine.jpg',
         title: 'This War of Mine',
         year: '2014',
-        category: ['Management', 'Survival'],
+        category: category[0],
         developer: '11 bit studios',
         publisher: '11 bit studios',
         description: 'In This War of Mine for the first time you do not play as an elite soldier, but a group of civilians trying to survive in a besieged city; struggling with lack of necessities and constant danger.',
@@ -43,7 +116,7 @@ const collection = [
         img: './imgs/illu-batman-arkham-knight.jpg',
         title: 'Batman: Arkham Knight',
         year: '2015',
-        category: ['Action', 'Adventure'],
+        category: category[0],
         developer: 'Rocksteady Studios',
         publisher: 'Warner Bros. Interactive Entertainment',
         description: 'Batman™: Arkham Knight brings the award-winning Arkham trilogy from Rocksteady Studios to its epic conclusion. Developed exclusively for New-Gen platforms, Batman: Arkham Knight introduces Rocksteady\'s uniquely designed version of the Batmobile. The highly anticipated addition of this legendary vehicle, combined with the acclaimed gameplay of the Arkham series, offers gamers the ultimate and complete Batman experience as they tear through the streets and soar across the skyline of the entirety of Gotham City. In this explosive finale, Batman faces the ultimate threat against the city that he is sworn to protect, as Scarecrow returns to unite the super criminals of Gotham and destroy the Batman forever.',
@@ -56,7 +129,7 @@ const collection = [
         img: './imgs/illu-plague-inc-evolved.jpg',
         title: 'Plague Inc.: Evolved',
         year: '2016',
-        category: ['Simulation', 'Strategy'],
+        category: category[0],
         developer: 'Ndemic Creations',
         publisher: 'Ndemic Creations',
         description: 'Your pathogen has just infected \'Patient Zero\'. Now you must bring about the end of human history by evolving a deadly, global Plague whilst adapting against everything humanity can do to defend itself. Plague Inc: Evolved is a unique mix of high strategy and terrifyingly realistic simulation which is now available on PC, Mac and Linux via Steam. Over 130 million players have been infected by Plague Inc. already. Now, Plague Inc: Evolved combines the original critically acclaimed gameplay with significant, all-new features for PC, including multiplayer, user-generated content support, improved graphics and lots more. Get ready to take strategy gaming to the next level!',
@@ -69,7 +142,7 @@ const collection = [
         img: './imgs/illu-cat-quest.jpg',
         title: 'Cat Quest',
         year: '2017',
-        category: ['Action', 'RPG'],
+        category: category[0],
         developer: 'The Gentlbros',
         publisher: 'PQube Limited',
         description: 'Cat Quest is a 2d Open World RPG by The Gentlebros. Set in the fantastic and wonderful world of cats, play as a catventurer as you explore a massive continent crafted in the unique style of tapestry! Relive the good old days of exploring an overworld map as you raid dungeons for epic loot, complete quests and meet the many furry denizens of this world! Also check out the sequel!',
@@ -82,7 +155,7 @@ const collection = [
         img: './imgs/illu-the-first-tree.jpg',
         title: 'The First Tree',
         year: '2017',
-        category: ['Exploration', 'Storytelling'],
+        category: category[0],
         developer: 'David Wehle',
         publisher: 'David Wehle',
         description: 'A third-person exploration game centered around two parallel stories: a fox trying to find her missing family, and a young couple dealing with a tragedy in theirs. Players take control of the fox on a poignant and beautiful journey that crescendos at the source of life, and perhaps results in the understanding of death. Along the way, players can uncover artifacts and stories from the young couple’s life as they too become intertwined in the fox’s journey towards The First Tree.',
@@ -95,7 +168,7 @@ const collection = [
         img: './imgs/illu-monster-hunter-world.jpeg',
         title: 'Monster: Hunter World',
         year: '2018',
-        category: ['Action', 'RPG'],
+        category: category[0],
         developer: 'Capcom',
         publisher: 'Capcom',
         description: 'As a hunter, you\'ll take on quests to hunt monsters in a variety of habitats. Take down these monsters and receive materials that you can use to create stronger weapons and armor in order to hunt even more dangerous monsters.',
@@ -108,7 +181,7 @@ const collection = [
         img: './imgs/illu-spiritfarer.jpg',
         title: 'Spiritfarer',
         year: '2020',
-        category: ['Management', 'Adventure'],
+        category: category[0],
         developer: 'Thunder Lotus Games',
         publisher: 'Thunder Lotus Games',
         description: 'Play as Stella, ferrymaster to the deceased, a Spiritfarer. Build a boat to explore the world, then befriend and care for spirits before finally releasing them into the afterlife.',
@@ -121,7 +194,7 @@ const collection = [
         img: './imgs/illu-phasmophobia.jpg',
         title: 'Phasmophobia',
         year: '2020',
-        category: ['Horror', 'Investigation'],
+        category: category[0],
         developer: 'Kinetic Games',
         publisher: 'Kinetic Games',
         description: 'Phasmophobia is an online 4 player co-op horror with VR support. Paranormal activity is on the rise and it is up to your team to investigate and end the nightmare.',
@@ -153,27 +226,29 @@ for (let element of collection){
 
     //list tags
     const newList = document.createElement('ul');
-    newList.className = '';
-    
-    const newTag = document.createElement('li');
-    newTag.className = 'has-text-weight-bold tag is-warning';
+
+    for (let elemTag of category){
+        const newTag = document.createElement('li');
+        newTag.className = elemTag.style;
+        newTag.textContent = elemTag.name;
+
+        newList.appendChild(newTag);
+    }
 
     //developer & publisher
     const newDevPub = document.createElement('p');
     newDevPub.className = 'mb-4';
-    
+
     const newSpanDev = document.createElement('span');
     newSpanDev.className = 'has-text-weight-semibold';
+    newSpanDev.textContent = ('Developer: ');
     
     const newSpanPub = document.createElement('span');
     newSpanPub.className = 'has-text-weight-semibold';
-        // newSpanDev.textContent = 'Developer: ';
-        // newSpanPub.textContent = 'Publisher: ';
-    newDevPub.textContent = (
-        newSpanDev.textContent + 
-        element.developer + ' ; ' +
-        newSpanPub.textContent + 
-        element.publisher);
+    newSpanPub.textContent = ('Publisher: ');
+
+    newDevPub.textContent = (newSpanDev + element.developer + ' ; ' + newSpanPub + element.publisher);
+
     
     //div overflow description + description
     const newDivDescription = document.createElement('div');
@@ -213,7 +288,6 @@ for (let element of collection){
         newFig.appendChild(newImg);
         //list tags
         newSection.appendChild(newList);
-        newList.appendChild(newTag);
         //developer & publisher
         newSection.appendChild(newDevPub);
         newDevPub.appendChild(newSpanDev);
